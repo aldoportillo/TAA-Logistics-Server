5af632182f438083b056097187dd8741d4ddeee7
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-03-24 18:58:19 -0500

    Trigger mailer in model callbacks

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,2 +1,9 @@
 class Inquiry < ApplicationRecord
+    after_create :send_new_inquiry_email
+
+    private
+
+    def send_new_inquiry_email
+        InquiryMailer.new_inquiry_email(self).deliver_later
+    end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,5 @@
 class Quote < ApplicationRecord
+  after_create :send_new_quote_email
 
     def self.ransackable_attributes(auth_object = nil)
         %w(contact_name company_name)
@@ -8,4 +9,10 @@ class Quote < ApplicationRecord
         # List any associations you want to be searchable, if any
         []
       end
+
+    private
+
+    def send_new_quote_email
+      QuoteMailer.new_quote_email(self).deliver_later
+    end
 end
