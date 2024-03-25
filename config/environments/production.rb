676a11525833cd12ade5219f2e35f0971833b49e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-03-25 13:25:54 -0500

    Leave a space for production before deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,12 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Email Delivery Settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    # Fill in later
+  }
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
