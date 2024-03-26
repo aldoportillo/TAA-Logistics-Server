74fa18a8b8b8e93004682f69ae9e321ab4ada19a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-03-26 16:50:28 -0500

    configure cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:5173' # Will need to change this to the actual front end URL
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head]
-    end
-  end
-  
+  allow do
+    origins 'https://taa-logistics-client.vercel.app'
+    resource '*',
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :
