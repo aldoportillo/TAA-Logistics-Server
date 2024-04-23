b92069f4bfde923b782aa6f9826502e9c17e8048
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-04-23 13:37:01 -0500

    Create events and images table

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,7 @@
+class ImagesController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :applications
   resources :quotes
   devise_for :users
+
+  resources :events do
+    resources :images, only: [:create, :destroy]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
