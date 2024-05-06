9eea488973be9a399d11313f4d0dd42e1ccb3ec3
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-05-06 16:58:20 -0500

    Add demo domain to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS_ORIGIN'] || 'http://localhost:5173'
+    origins ENV['CORS_ORIGIN'] || 'https://taa-logistics-client.vercel.app/'
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
