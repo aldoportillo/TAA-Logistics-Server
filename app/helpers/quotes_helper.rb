e0a427b113c619475bf505785570b8f4cb34c5e3
aldo portillo <94483081+aldoportillo@users.noreply.github.com>
2025-06-12 23:45:30 -0500

    modify helper

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -1,2 +1,13 @@
 module QuotesHelper
+  def calculate_line_haul(miles, rate)
+    PricingMatrix.line_haul_for_miles(miles, rate, fuel_included: false)
+  end
+
+  def calculate_line_haul_including_fuel(miles, rate)
+    PricingMatrix.line_haul_for_miles(miles, rate, fuel_included: true)
+  end
+
+  def calculate_fuel_surcharge(line_haul, fsch_percent)
+    ((fsch_percent.to_f / 100) * line_haul).round(2)
+  end
 end
